a60a9a4bc6921d770f50909552f3779394f98917
BushraPrapti <bushraprapti151288@gmail.com>
2022-10-15 10:47:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Main from './components/Layout/Main';
+import Practices from './components/Practices/Practices';
+import StatChart from './components/StatChart/StatChart';
+import Topics from './components/Topics/Topics';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Home></Home>
+        },
+        {
+          path: '/practices/:practicesId',
+          loader: ({ params }) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.practicesId}`),
+          element: <Practices></Practices>
+        },
+        {
+          path: '/topics',
+          element: <Topics></Topics>,
+          // children: [
+          //   {
+          //     path: '/',
+          //     loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          //     element: <Home></Home>
+          //   }
+          // ]
+
+        },
+        {
+          path: '/statchart',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <StatChart></StatChart>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <div><h4>Ops</h4></div>
+
+    }
+
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
